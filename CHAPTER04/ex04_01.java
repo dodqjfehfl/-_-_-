11a65793988e9f5fe8b21798d54b7593735b18b3
dodqjfehfl <52148557+dodqjfehfl@users.noreply.github.com>
2020-11-10 23:40:16 +0900

    11/10
    Chapter04

---
+++ CHAPTER04/ex04_01.java
@@ -0,0 +1,33 @@
+public class ex04_01{
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader()
+        int N=
+//         N=int(input())
+// x,y=1,1
+// plans=input().split()
+
+// dx=[0,0,-1,1]
+// dy=[-1,1,0,0]
+// move_types=['L','R','U','D']
+
+// for plan in plans:
+//     for i in range(4):
+//         if plan==move_types[i]:
+//             x+=dx[i]
+//             y+=dy[i]
+//             if x<1:
+//                 x=1
+//             if x>N:
+//                 x=N
+//             if y<1:
+//                 y=1
+//             if y>N:
+//                 y=N
+
+// print(x, y)
+
+    }
+}
+
+//5
+//R R R U D D
